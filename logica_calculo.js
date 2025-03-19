853f4d44bcf53ea1d6cfc6f3fc497d27cc25e741
nhengler <niko_las1934@hotmail.com>
2025-03-19 03:12:05 -0300

    primeira atualizacao apos config do git

--- logica_calculo.js
+++ logica_calculo.js
@@ -10,13 +10,20 @@ function fazerCalculo(){
     }
 
     let valorResultado = 88 + (13.397 * valorPeso) + (4.799 * valorAltura) - (5.677 * valorIdade);
-    document.getElementById('resultado').innerHTML = "Resultado: " + valorResultado.toFixed(2) + "<br><br>";
+    document.getElementById('resultado').innerHTML = "Resultado: " + valorResultado.toFixed(2) + "<br>";
     document.getElementById('resultado').innerHTML += "Com base nos seus resultados, fizemos um cardápio especial para você. Clique no botão abaixo para visualizar.";
-    //adição de botão qu mostra o cardáipo
-}    
-
     
+    let botaoSugestao = document.createElement('button'); // cria o botão de sugestão no HTML
+    botaoSugestao.textContent = "Visualizar cardápio";
+    botaoSugestao.id = "exibirCardapio"; // id para editar no css
+    resultado.appendChild(botaoSugestao); // método que adiciona "botaoSugestao" na div "resultado" 
+    botaoSugestao.addEventListener('click', function(){
+        if(valorResultado <= 2000){
+            window.open("cardapio1.html"); // abre cardapio1.html em outra guia
+        }   else if (valorResultado > 2000 && valorResultado <= 2500){
+            window.open("cardapio2.html"); // abre cardapio2.html em outra guia
+        }
+        });
+    }    
     
-
-
 document.getElementById('exibirResultado').addEventListener('click', fazerCalculo);
