ff8ac548cffd9c17cb31493ba20cc3713ba6a324
unknown <niko_las1934@hotmail.com>
2025-08-21 03:49:07 -0300

    importação das rotas para o server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,13 @@
+const bodyParser = require("body-parser");
 const app = require('./src/app'); // importa o app já configurado
+
+// middlewares
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// importar rotas 
+const userRoutes = require("./routes/users");
+app.use("/users", userRoutes);
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
